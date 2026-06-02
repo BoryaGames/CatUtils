e5a26c4daefe191675059655fcbf3ea0795b9d15
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2026-06-02 21:34:09 +0500

    Улучшение

--- modules/minecraft/player/module.js
+++ modules/minecraft/player/module.js
@@ -5,10 +5,12 @@
     <img src="download.png" width="32px" height="32px">
     <br>
     <p id="outputData" style="color: white;">(Информации нет.)</p>
+    <canvas id="skin" width="300px" height="400px"></canvas>
     <br><br><br>
     <button data-call="load">Загрузить</button>
   `,
-  "load": async ({ player, outputData }) => {
+  "load": async ({ player, outputData, skin }) => {
+    outputData.innerHTML = `<font color="yellow">Загрузка...</font>`;
     try {
       var result = await fetch(`https://playerdb.co/api/player/minecraft/${player.value}`, {
         "headers": {
@@ -21,7 +23,12 @@
     }
     if (result.success) {
       outputData.innerHTML = `Игрок <font color="lime">${result.data.player.username}</font> с UUID <font color="lime">${result.data.player.id}</font> существует.`;
-      return;
+      return new skinview3d.SkinViewer({
+		    "canvas": skin,
+		    "width": 300,
+		    "height": 400,
+		    "skin": result.data.player.skin_texture
+	    });
     }
     if (player.value.match(/^[0-9a-f]{8}-?[0-9a-f]{4}-?[0-5][0-9a-f]{3}-?[089ab][0-9a-f]{3}-?[0-9a-f]{12}$/i)) {
       outputData.innerHTML = `Игрока с таким UUID <font color="red">не существует</font>.`;
