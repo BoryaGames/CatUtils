1dfbd2f374d0a95aa7950db97ecc823abb2c362d
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2026-06-02 22:08:43 +0500

    Исправление

--- modules/minecraft/server/module.js
+++ modules/minecraft/server/module.js
@@ -20,7 +20,7 @@
     outputData.innerHTML = `<font color="yellow">Загрузка...</font>`;
     icon.style.display = "none";
     try {
-      var result = await fetch(`https://api.mcsrvstat.us${(document.querySelector(".blockselect.selected").innerText == "Bedrock" ? "/bedrock" : "")}/3/${encodeURIComponent(address)}`, {
+      var result = await fetch(`https://api.mcsrvstat.us${(document.querySelector(".blockselect.selected").innerText == "Bedrock" ? "/bedrock" : "")}/3/${encodeURIComponent(address.value)}`, {
         "headers": {
           "User-Agent": "CatUtils (https://catutils.topcatto.xyz, v0.0.1)"
         }
@@ -29,7 +29,7 @@
       outputData.innerHTML = `<font color="red">Не удалось</font> получить информацию.`;
       return;
     }
-    outputData.innerHTML = `Сервер: <font color="lime">${result.ip}</font>${result.hostname ? ` (<font color="lime">${result.hostname}</font>)` : ""}<br />Статус: ${result.online ? `<font color="lime">Онлайн</font>` : `<font color="red">Оффлайн</font>`}<br />Версия: <font color="lime">${result.version}</font>${result.software ? `<br />Ядро: <font color="lime">${result.software}</font>` : ""}${result.players ? `<br />Игроки: <font color="lime">${result.players.online} / ${result.players.max}</font>` : ""}`;
+    outputData.innerHTML = `Сервер: <font color="lime">${result.ip}</font>${result.hostname ? ` (<font color="lime">${result.hostname}</font>)` : ""}<br />Статус: ${result.online ? `<font color="lime">Онлайн</font>` : `<font color="red">Оффлайн</font>`}${result.version ? `<br />Версия: <font color="lime">${result.version}</font>` : ""}${result.software ? `<br />Ядро: <font color="lime">${result.software}</font>` : ""}${result.players ? `<br />Игроки: <font color="lime">${result.players.online} / ${result.players.max}</font>` : ""}`;
     if (result.icon) {
       icon.style.display = "block";
       icon.src = result.icon;
