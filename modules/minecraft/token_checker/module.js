eb948cb958668d491961d6c8c1889d7b580a440c
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2026-06-04 19:50:41 +0500

    Добавил проверку токена

--- modules/minecraft/token_checker/module.js
+++ modules/minecraft/token_checker/module.js
@@ -9,7 +9,7 @@
     <button data-call="scan">Проверить</button>
   `,
   "scan": async ({ token, outputData }) => {
-    var tokenData = token.value.match(/^([a-zA-Z0-9\-_]+)\.([a-zA-Z0-9\-_]+)\.[a-zA-Z0-9\-_]+$/);
+    var tokenData = token.value.match(/^([a-zA-Z0-9\-_]+)\.([a-zA-Z0-9\-_]+)\.([a-zA-Z0-9\-_]+)$/);
     if (!tokenData) {
       outputData.innerHTML = `Это выглядит как <font color="red">невозможный</font> JWT-токен.`;
       return;
@@ -35,8 +35,28 @@
       outputData.innerHTML = `Это выглядит как <font color="red">повреждённый</font> токен доступа Minecraft.`;
       return;
     }
+
+    // From https://api.minecraftservices.com/publickeys
+    const minecraftPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4S4G8fJJw4hLRME8zM2aBqaQRqYs0TlU8N+sZl0MUgtyFw0KAXnzbOPH7yu6qbqsGYVKF/D+fUg+49PhFmxEsylvmj/yeRwdp3yFGME/BVfL06zFmAw+rGaxqubcswzIO8ByUtjQOmlnrzj4zvhNSYJmwNbTIKrKNlSHYvYZbUDRquH9yMDOKnvGAMMDGttFrM300mVznRgaTEzU9aPqMvj0YxtxUcGIQTar0TBQa7NzEAr59u5VVx5s6naS6QVBrMc6e32f38enVkNFZQT87KlCb2B6ziPmbaRzWWs4qcHsHz8BUCKplo5iu/ePtwaa5AaVT27Lnv+KzS46eyf1CwIDAQAB";
+
+    function decodeBinary(b64) {
+      var bin = atob(b64.replace(/-/g, "+").replace(/_/g, "/"));
+      var bytes = new Uint8Array(bin.length);
+      for (var i = 0; i < bin.length; i++) {
+        bytes[i] = bin.charCodeAt(i);
+      }
+      return bytes.buffer;
+    }
     var status = "";
-    if (Date.now() > (metadata.exp * 1000)) {
+    var data = (new TextEncoder()).encode(`${tokenData[1]}.${tokenData[2]}`);
+    var sig = decodeBinary(tokenData[3]);
+    var key = await crypto.subtle.importKey("spki", decodeBinary(minecraftPublicKey), {
+      "name": "RSASSA-PKCS1-v1_5",
+      "hash": "SHA-256"
+    }, false, ["verify"]);
+    if (!await crypto.subtle.verify("RSASSA-PKCS1-v1_5", key, sig, data)) {
+      status = `Этот токен <font color="red">не настоящий</font>.`;
+    } else if (Date.now() > (metadata.exp * 1000)) {
       status = `Этот токен <font color="red">устарел</font>.`;
     } else if (Date.now() < (metadata.nbf * 1000)) {
       status = `На устройстве <font color="yellow">неправильное время</font>, невозможно проверить токен.`;
